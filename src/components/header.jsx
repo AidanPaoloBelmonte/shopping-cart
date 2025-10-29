7f9cbf7fff3fa4cdc37536e40e6907f20f82ee2f
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-10-29 11:24:30 +0800

    feat: Added notification area to the right of the navigation bar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,32 +7,39 @@ export default function Header({ currentContext, cartItems }) {
   const cartNumberDisplay = Math.ceil(Math.floor(cartItems.length, 99), 0);
 
   return (
-    <nav className="mainNav">
-      <h1>ShopDigi</h1>
-      <ol>
-        <li className="navHome">
-          <div className="navContainer centerAligned">
-            <Link to="home" className="navContent animated">
-              Home
-            </Link>
-          </div>
-        </li>
-        <li className="navShop">
-          <div className="navContainer centerAligned">
-            <Link to="shop" className="navContent animated">
-              Shop
-            </Link>
-          </div>
-        </li>
-        <li className="navCart">
-          <div className="navContainer centerAligned">
-            <Link to="cart" className="navContent centerAligned">
-              <span>Cart</span>
-              <span className={cartNumberClassName}>{cartNumberDisplay}</span>
-            </Link>
-          </div>
-        </li>
-      </ol>
-    </nav>
+    <section className="headerSection">
+      <nav className="mainNav">
+        <h1>ShopDigi</h1>
+        <ol>
+          <li className="navHome">
+            <div className="navContainer centerAligned">
+              <Link to="home" className="navContent animated">
+                Home
+              </Link>
+            </div>
+          </li>
+          <li className="navShop">
+            <div className="navContainer centerAligned">
+              <Link to="shop" className="navContent animated">
+                Shop
+              </Link>
+            </div>
+          </li>
+          <li className="navCart">
+            <div className="navContainer centerAligned">
+              <Link to="cart" className="navContent centerAligned">
+                <span>Cart</span>
+                <span className={cartNumberClassName}>{cartNumberDisplay}</span>
+              </Link>
+            </div>
+          </li>
+        </ol>
+      </nav>
+      <div className="notifArea centerAligned">
+        <h4 className="centerAligned">
+          Everything from Everywhere at your fingertips.
+        </h4>
+      </div>
+    </section>
   );
 }
