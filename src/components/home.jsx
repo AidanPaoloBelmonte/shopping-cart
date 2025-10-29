9d544e9fafe6ce28d137f9822f89d0f42e46d45a
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-10-29 13:34:04 +0800

    feat: Added basic layout for the Home Page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useOutletContext } from "react-router";
 
+import "./home.css";
+
 export default function Home() {
   const { setNotifArea } = useOutletContext();
 
@@ -8,5 +10,20 @@ export default function Home() {
     setNotifArea(0);
   });
 
-  return <>Home</>;
+  return (
+    <section className="homeSection">
+      <div className="imageCarousel">
+        <div className="window"></div>
+        <div className="about">
+          <h2>New Summer Sale!</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque
+            autem ullam quos dicta. Quos quis, odio in atque commodi asperiores
+            delectus aliquid soluta iusto distinctio modi laboriosam eos dolorum
+            sint!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
